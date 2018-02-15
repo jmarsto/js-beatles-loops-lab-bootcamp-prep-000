e8156402198daa560ac242c7759b43972ab4ec0b
jackdyett@gmail.com Marston <jackdyett@gmail.com>
2018-02-15 23:46:53 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+function theBeatlesPlay(musicians, instruments) {
+  var newStrings = []
+  for (var i = 0; i < musicians.length; i++) {
+    newStrings[i] = `${musicians[i]} plays ${instruments[i]}`
+  }
+  return newStrings
+}
+var i = 0
+
+function johnLennonFacts(array) {
+  var newFacts = []
+  while (i < array.length) {
+    newFacts[i] = `${array[i]}!!!`
+    i++
+  }
+  return newFacts
+}
+
+function iLoveTheBeatles(num) {
+  var arr = []
+  do {
+    arr[num] = "I love the Beatles"
+  num++
+  }
+  while (num < 15)
+}
